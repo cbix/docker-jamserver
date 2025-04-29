3ae09803c19d605e55a3eda70d117c0f78dce68f
Florian Hülsmann <fh@cbix.de>
2025-04-29 22:57:21 +0200

    Update apkbuilds

--- jamulus/entrypoint.sh
+++ jamulus/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-exec jamulus --multithreading \
+exec jamulus --multithreading --enableipv6 \
   ${DIRECTORYADDRESS:+--directoryaddress "$DIRECTORYADDRESS"} \
   ${SERVERINFO:+--serverinfo "$SERVERINFO"} \
   ${WELCOMEMESSAGE:+--welcomemessage "$WELCOMEMESSAGE"} \
